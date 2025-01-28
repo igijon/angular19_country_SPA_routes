dd5eb94de928f5881b516af2c078f38d1a253215
Inma Gijón <igijoninf@gmail.com>
2025-01-28 14:26:34 +0100

    Mostrar y ocultar LoadingSpinner

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -11,14 +11,17 @@ import { Country } from '../../interfaces/country';
 export class ByCapitalPageComponent {
 
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor ( private countriesService: CountriesService){}
 
   searchByCapital(term: string) {
+    this.isLoading = true;
     //Nos tenemos que suscribir al observable para que se emita
     this.countriesService.searchCapital( term )
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
 
   }
