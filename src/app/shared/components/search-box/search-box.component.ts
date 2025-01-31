e8de6b316de4dd1736bf6945ce3ea3e3eb18eb81
Inma Gijón <igijoninf@gmail.com>
2025-01-31 12:52:52 +0100

    Debounce sin presionar enter

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -6,15 +7,38 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  //Es un tipo de observable manual. Es rxjs
+  private debouncer: Subject<string> = new Subject<string>();
+
   @Input()
   public placeholder: string = "";
 
   @Output()
   public onValue = new EventEmitter<string>();
 
+  @Output()
+  public onDebounce = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    //Se ejecuta después del constructor
+    this.debouncer
+      .pipe(
+        //Si estoy un 400 ms sin recibir información entonces lo emite
+        debounceTime(400)
+    )
+    .subscribe( value => {
+      this.onDebounce.emit(value);
+    });
+  }
+
   emitValue( value: string ): void {
     this.onValue.emit(value);
+  }
+
+  onKeyPress( searchTerm: string ) {
+    this.debouncer.next( searchTerm );
   }
 
 }
